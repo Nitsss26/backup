d727a1969f0cc4c7a8716c95203bc5169c7c7499
Nitesh <niteshshr123@gmail.com>
2025-07-24 02:01:03 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -143,7 +143,7 @@ export default function BooksPage() {
             <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
               {filteredBooks.map(book => (
                 <BookCard 
-                  key={book.id} 
+                  key={book._id} 
                   book={book} 
                   distance={book.distance}
                 />
